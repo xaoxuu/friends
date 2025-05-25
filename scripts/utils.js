7e4c6a25e0b6aaa8e9dd125e43c4ff36e7e128e3
xaoxuu <git@xaoxuu.com>
2025-05-25 16:55:02 +0800

    add log

--- scripts/utils.js
+++ scripts/utils.js
@@ -87,7 +87,7 @@ export class IssueManager {
         sort: 'created',
         direction: 'desc'
       });
-      
+      logger('info', `Fetched ${issues.length} issues`, issues.map(item => item.number).join(','));
       // 过滤掉包含 exclude_labels 中定义的标签的 Issue
       const filteredIssues = issues.filter(issue => {
         const issueLabels = issue.labels.map(label => label.name);
@@ -100,6 +100,7 @@ export class IssueManager {
         return issue.body?.includes(filterConfig.include_keyword);
       });
       
+      logger('info', `Filtered ${issues.length}`, keywordFilteredIssues.map(item => item.number).join(','));
       return keywordFilteredIssues.map(issue => ({
         url: issue.body?.match(/"url":\s*"([^"]+)"/)?.at(1),
         issue_number: issue.number,
